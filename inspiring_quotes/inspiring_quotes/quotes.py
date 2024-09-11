bf69fbf6e5c9ddbadd11eea2e739478034f84378
cyberdeeb <83480723+cyberdeeb@users.noreply.github.com>
2024-09-11 13:44:57 -0700

    Correct json error

--- inspiring_quotes/inspiring_quotes/quotes.py
+++ inspiring_quotes/inspiring_quotes/quotes.py
@@ -2,8 +2,6 @@ import json
 import random
 
 def get_random_quote():
-     with open('inspiring_quotes/quotes.json', 'r') as file:
+     with open('inspiring_quotes/inspiring_quotes/quotes_list.json', 'r') as file:
         quotes = json.load(file)
         return random.choice(quotes)
-
-print(get_random_quote())
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='inspiring_quotes',
-    version='0.3.5',
+    version='0.4.0',
     description='A simple Python package for retrieving random inspirational quotes.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
