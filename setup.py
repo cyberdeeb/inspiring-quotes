fdd30af3277eba61763c210bc8bda084fb7b89dc
cyberdeeb <83480723+cyberdeeb@users.noreply.github.com>
2024-09-11 12:59:04 -0700

    Updating file structure and uploading version 1.5

--- setup.py
+++ setup.py
@@ -1,18 +1,25 @@
-# setup.py
 from setuptools import setup, find_packages
 
 setup(
-    name="inspiring_quotes",
-    version="0.1",
-    description="A simple Python package for inspirational quotes.",
-    author="Your Name",
-    author_email="your.email@example.com",
+    name='inspiring_quotes',
+    version='1.5',
+    description='A simple Python package for retrieving random inspirational quotes.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='cyberdeeb',
+    author_email='abrahamdeeb@gmail.com',
+    url='https://github.com/cyberdeeb/inspiring-quotes',
     packages=find_packages(),
-    include_package_data=True,
-    install_requires=[],
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
+    keywords='quotes inspirational motivational wisdom positivity self-improvement',
+    python_requires='>=3.6',
 )
