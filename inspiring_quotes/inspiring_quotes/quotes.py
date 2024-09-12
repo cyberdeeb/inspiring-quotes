7e9a59197aa7e182bc9b0f65071a11695000c98a
cyberdeeb <83480723+cyberdeeb@users.noreply.github.com>
2024-09-12 08:13:02 -0700

    Fixed json file error

--- inspiring_quotes/inspiring_quotes/quotes.py
+++ inspiring_quotes/inspiring_quotes/quotes.py
@@ -1,14 +1,18 @@
 import json
 import random
+import importlib.resources as pkg_resources  # Importlib resources
 
 def get_random_quote():
     try:
-        with open('inspiring_quotes/inspiring_quotes/quotes_list.json', 'r') as file:
+        # Using importlib.resources.files to get the file
+        with pkg_resources.files(__package__).joinpath('quotes_list.json').open('r') as file:
             quotes = json.load(file)
             return random.choice(quotes)
     except FileNotFoundError:
-        print("The file 'quotes.json' does not exist.")
+        print("The file 'quotes_list.json' does not exist.")
         return None
     except json.JSONDecodeError:
         print("Error decoding JSON. Please check the file format.")
         return None
+
+print(get_random_quote())
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='inspiring_quotes',
-    version='0.6.0',
+    version='0.6.5',
     description='A simple Python package for retrieving random inspirational quotes.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
@@ -10,16 +10,18 @@ setup(
     author_email='abrahamdeeb@gmail.com',
     url='https://github.com/cyberdeeb/inspiring-quotes',
     packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'inspiring_quotes': ['quotes_list.json'],
+    },
     classifiers=[
-        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
     ],
     keywords='quotes inspirational motivational wisdom positivity self-improvement',
-    python_requires='>=3',
+    python_requires='>=3.6',
 )
