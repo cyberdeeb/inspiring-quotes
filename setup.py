5619000830ea9f704118c6213c113841c7e9a0f6
cyberdeeb <83480723+cyberdeeb@users.noreply.github.com>
2024-09-12 08:46:03 -0700

    initialize project

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='inspiring_quotes',
-    version='0.8.5',
+    version='0.2.5',
     description='A simple Python package for retrieving random inspirational quotes.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
